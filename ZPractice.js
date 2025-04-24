bf172d963a70bb6423891cf56383fe09eab930b3
Parmod Kumar SnDigitech <parmod.sndigitech@gmail.com>
2025-04-24 11:18:51 +0530

    Update ZPractice.js

--- ZPractice.js
+++ ZPractice.js
@@ -4,9 +4,15 @@
 npx react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res
 ./gradlew assembleDebug
 
+      ./gradlew bundleDebug
+
 
 ./gradlew assembleRelease
 
+.abb  file 
+
+cd android
+./gradlew bundleRelease
 
 
 
