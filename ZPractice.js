cbed907efa8a22ded6be557055a6e0d21e12f920
Parmod Kumar SnDigitech <parmod.sndigitech@gmail.com>
2025-06-12 22:14:27 +0530

    Update ZPractice.js

--- ZPractice.js
+++ ZPractice.js
@@ -10,6 +10,8 @@ taskkill /PID 1234 /F
 
 
 
+mkdir android\app\src\main\assets
+mkdir android\app\src\main\res
 
 npx react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res
 ./gradlew assembleDebug
